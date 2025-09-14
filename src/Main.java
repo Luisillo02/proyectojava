65cc4ca2356e9775b021666c22ce7b2a1c5ec743
Luisillo02 <luis.rodrigo.5mega@gmail.com>
2025-09-14 15:53:56 -0300

    Se hace prueba de DAOs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.sql.Connection;
-
+import java.sql.Date;
 public class Main {
     public static void main(String[] args){
         Connection conn = ConectorBD.getConnection();
@@ -11,13 +11,37 @@ public class Main {
         }
         UsuarioDao UsuarioDao = new  UsuarioDao();
         // Insertar usurio
-        Usuario nuevo = new Usuario(0,"Pedro","Sanchez","Pedro@gmail.com");
+        Usuario nuevo = new Usuario(0,"Javier","Milei","Elleon@gmail.com");
         UsuarioDao.agregarUsuario(nuevo);
 
         // Lista de usuario 
         for (Usuario u : UsuarioDao.listaRUsuarios()){
-            System.out.println(u.getid_Usuario()+" - " + u.getNombre()+ " " + u.getApellido());
+            System.out.println(u.getid_Usuario()+" - " + u.getNombre()+ " - " + u.getApellido());
         }
-    }
+        // Insertar Tarea
+        TareasDao TareasDao = new TareasDao();
+        Tareas nuevaTareas = new Tareas(1, "Tarea 01", "Sacar la basura", "Semanal  ", new Date ( System.currentTimeMillis()) , true);
+        TareasDao.agregarTarea(nuevaTareas);
 
+        for (Tareas t : TareasDao.listaRTarea()){
+            System.out.println(t.getid_tarea()+ "-" + t.getnombre()+ "-" +t.getdescripcion()+ "-"+t.getfrecuencia()+ "-" + t.getUltima_fecha_realizada()+ "-" + t.isestado());
+        }
+
+        HogarDao HogarDao = new HogarDao();
+        // insertamos hogar
+        Hogar nuevoHogar = new Hogar(0, "La casona", "Av El libertador 234");
+        HogarDao.agregarHogar(nuevoHogar);
+        for ( Hogar h : HogarDao.listaRHogar()){
+            System.out.println(h.getIdHogar()+ " - " + h.getNombre()+ "-" + h.getDireccion());
+        }
+        // Insertar Asignacion
+        AsignacionDao AsignacionDao = new AsignacionDao();
+        Asignacion nuevagAsignacion = new Asignacion(0, 0, 0, new Date( System.currentTimeMillis()), new Date (2025,12,12),"Activo");
+        AsignacionDao.agregarAsignacion(nuevagAsignacion);
+        for (Asignacion a : AsignacionDao.listaRAsignacion()){ 
+            System.out.println(a.getid_asignacion()+ " - " + a.getid_usuario()+ "-" + a.getid_tarea()+ "-" + a.getfecha_asignacion()+ "-" + a.getfecha_realizacion());
+        }
+
+    }
 }
+
